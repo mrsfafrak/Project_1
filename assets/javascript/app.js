d43e88be8dc8fd4083e654fe0b53bd482e09b5e8
Tori <victoria.fafrak@gmail.com>
2019-03-12 19:10:11 -0400

    intital page setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+// petfinder notes:
+// https://api.petfinder.com/pet.find?key=129bc9340b07c13a3cae63a8fd9f07e6&format=json&animal=dog&location=44236
+$("button").on("click", function(event){
+    event.preventDefault();
+    // clear input cells
+    var queryURL
+})
